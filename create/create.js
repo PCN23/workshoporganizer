bd57bb5a906f437d05635acb2ece75a5d4638594
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-24 18:13:49 -0700

    supabase error

--- create/create.js
+++ create/create.js
@@ -7,22 +7,25 @@ const form = document.querySelector('car-form');
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(form);
+
     await createParticipant({
         name: data.get('class'),
         contact: data.get('participants'),
+        
     });
     window.location.href = '../other-page';
     form.reset();
 });
 
 window.addEventListener('load', async (e) => {
-    const cars = await getClasses();
+    // e.preventDefault();
     const selectEl = document.querySelector('select');
+    const cars = await getClasses();
     for (let car of cars) {
-        const option = document.createElement('option');
-        option.value = car.id;
-        option.textContent = car.name;
-        selectEl.append(option);
+        const optionEl = document.createElement('option');
+        optionEl.value = car.id;
+        optionEl.textContent = car.name;
+        selectEl.append(optionEl);
     }
 });
 
